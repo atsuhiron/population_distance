568c7baedbf219a90ca9f3a987ed3424c400c674
atsuhiron <atsuronreplaced@gmail.com>
2023-05-25 20:23:36 +0900

    Add argmin_jit

--- measure_jit.py
+++ measure_jit.py
@@ -18,6 +18,20 @@ def calc_dist_matrix_2d_jit(loc_larger: np.ndarray, loc_smaller: np.ndarray) -> 
     return np.sqrt(np.sum(np.square(_loc_larger_mat - _loc_smaller_mat), axis=2))
 
 
+@numba.jit("i8[:](f8[:, :])", cache=True, nopython=True)
+def argmin_2d_jit(arr: np.ndarray) -> np.ndarray:
+    cur_min = 1.79769313486231e+308
+    min_row_index = -1
+    min_col_index = -1
+    for irow in range(len(arr)):
+        for icol in range(len(arr[0])):
+            if arr[irow, icol] < cur_min:
+                cur_min = arr[irow, icol]
+                min_row_index = irow
+                min_col_index = icol
+    return np.array([min_row_index, min_col_index], dtype=np.int64)
+
+
 @numba.jit("f8[:](f8[:, :])", cache=True, nopython=True)
 def find_proximal_jit(dist_matrix: np.ndarray) -> np.ndarray:
     max_val = 1.79769313486231e+308
@@ -26,9 +40,9 @@ def find_proximal_jit(dist_matrix: np.ndarray) -> np.ndarray:
     proximal_distance = np.zeros(h_size, dtype=np.float64)
     for _ in range(h_size):
         # find the most proximal
-        min_index = np.argmin(dist_matrix)
-        min_row = min_index // h_size
-        min_col = min_index % h_size
+        min_index = argmin_2d_jit(dist_matrix)
+        min_row = min_index[0]
+        min_col = min_index[1]
 
         # log distance
         proximal_distance[min_col] = dist_matrix[min_row, min_col]
@@ -37,6 +51,7 @@ def find_proximal_jit(dist_matrix: np.ndarray) -> np.ndarray:
         dist_matrix[:, min_col] = max_val
 
     return proximal_distance
+
 
 
 if __name__ == "__main__":
@@ -49,3 +64,6 @@ if __name__ == "__main__":
     print(ret2)
     ret3 = find_proximal_jit(ret2)
     print(ret3)
+
+    argmin_ret = argmin_2d_jit(np.random.random((10, 3)))
+    print(argmin_ret)
